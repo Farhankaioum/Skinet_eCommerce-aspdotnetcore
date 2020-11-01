60c1f9ce6a6b5f248eed02c54e44cbb307f0a76f
Farhan Kaioum <km1472233@gmail.com>
2020-11-01 19:52:56 +0600

    Product entity, EF core configuration added

---
+++ Skinet.API/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Skinet.API.Data;
+
+namespace Skinet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var datas = await _context.Products.ToListAsync();
+
+            return Ok(datas);
+        }
+
+        [HttpGet("{id}")]
+        public async  Task<IActionResult> GetProductById(int id)
+        {
+            var data = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+            return Ok(data);
+        }
+    }
+}
---
+++ Skinet.API/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Skinet.API.Entities;
+
+namespace Skinet.API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
