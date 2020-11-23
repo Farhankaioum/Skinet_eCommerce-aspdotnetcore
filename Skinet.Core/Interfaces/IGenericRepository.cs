6bc8956bc76cd56c67948b322b7057778c8edfd6
Farhan Kaioum <km1472233@gmail.com>
2020-11-23 10:36:51 +0600

    GenericBaseRepository added

---
+++ Skinet.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using Skinet.Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Skinet.Core.Interfaces
+{
+    public interface IGenericRepository<T> where T: BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+    }
+}
---
+++ Skinet.Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Skinet.Core.Entities;
+using Skinet.Core.Interfaces;
+using Skinet.Infrastucture.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Skinet.Infrastructure.Data
+{
+    public class GenericRepository<T> : IGenericRepository<T>
+        where T : BaseEntity
+    {
+        private readonly StoreContext _context;
+
+        public GenericRepository(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+    }
+}
